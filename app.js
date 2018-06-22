be3743982aeeb26bba4662a6af737b032c944c02
HubertRonald <HubertRonald@users.noreply.github.com>
2018-06-22 18:27:31 -0500

    Compressor

--- app.js
+++ app.js
@@ -68,3 +68,6 @@ function LunchCheckController($scope) {
 }
 
 })();
+
+// Verion: JavaScript Minifier/Compressor
+// !function(){"use strict";function e(e){e.names="",e.msg="",e.customStyle={},e.turnGreen=function(){e.customStyle.style={color:"green",border:"1px solid green"}},e.turnRed=function(){e.customStyle.style={color:"red",border:"1px solid red"}},e.myLunchChecker=function(){if(null==e.names&&(e.names=""),""==e.names)e.msg=" Please enter data first ",e.turnRed();else{var n=e.names,r=0;for(var o in n)o.replace(/\s/g,"").length&&r++;r>0&&r<=3?e.msg=" Enjoy! ":r>3&&(e.msg=" Too much! "),e.turnGreen()}}}angular.module("LunchCheck",[]).controller("LunchCheckController",e),e.$inject=["$scope"]}();
